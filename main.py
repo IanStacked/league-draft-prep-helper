0bd35b8f400b2901065a4534b2c64d067a487bb4
IanStacked <ian.rodgers156@gmail.com>
2025-10-24 14:31:54 -0700

    add import functionality for .env variables into main function

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+import os
+from dotenv import load_dotenv
+
 def main():
+    load_dotenv()
+    RIOT_API_KEY = os.getenv("RIOT_API_KEY")
+    GOOGLE_SHEETS_API_KEY = os.getenv("GOOGLE_SHEETS_API_KEY")
+    
     print("Hello from leaguehelper!")
 
 
