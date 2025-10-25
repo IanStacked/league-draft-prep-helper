736156423f1575b05466ed209f200d92432b3d25
IanStacked <ian.rodgers156@gmail.com>
2025-10-25 15:30:16 -0700

    functionality for riotid to data in sheet for single riotid

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import requests
 import gspread
 from dotenv import load_dotenv
+from collections import Counter
 
 def get_puuid(game_name, tag_line, riot_api_key):
     api_url = f"https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{game_name}/{tag_line}"
@@ -96,15 +97,28 @@ def collect_league_data(RIOT_API_KEY, game_name, tag_line):
     player_info = []
     puuid = get_puuid(game_name, tag_line, RIOT_API_KEY)
     recent_matches = get_recent_matches(puuid, RIOT_API_KEY)
-    mastery_info = get_player_mastery(puuid, RIOT_API_KEY)
+    #mastery_info = get_player_mastery(puuid, RIOT_API_KEY)
     champions_played = []
     for match in recent_matches:
         champions_played.append(extract_player_champ_info(get_match_details(match,RIOT_API_KEY), puuid))
+    champion_counts = Counter(champions_played)
+    sorted_champions_played_counts = champion_counts.most_common()
+    sorted_champions_played = [champion for champion, count in sorted_champions_played_counts]
     player_info.append({
         "game_name": game_name,
-        "champions_played": champions_played
+        "sorted_champions_played": sorted_champions_played
     })
     return player_info
+
+def print_info_to_sheets(worksheet_string, sh, player_info):
+    worksheet = sh.worksheet(worksheet_string)
+    #set header
+    worksheet.update_cell(1,1, player_info[0]["game_name"])
+    #fill in data
+    i = 2
+    for champ in player_info[0]["sorted_champions_played"]:
+        worksheet.update_cell(i, 1, champ)
+        i += 1
     
 def main():
     load_dotenv()
@@ -112,17 +126,17 @@ def main():
     print("Please input player information, format example: Game Name: frenzy, Tag Line: aja, for riotid frenzy#aja")
     game_name = input("Game Name: ",)
     tag_line = input("Tag Line: ",)
-    #player_info = collect_league_data(RIOT_API_KEY, game_name, tag_line)
+    player_info = collect_league_data(RIOT_API_KEY, game_name, tag_line)
     
     #google sheets integration here
     gc = gspread.service_account(filename='credentials.json')
     try:
+        #this key determines which sheet is opened
         sh = gc.open_by_key("1wp9h_LorMKCMWHLCfBOjq8WlZUuw2mgYlUqnJv7GeXY")
     except gspread.SpreadsheetNotFound:
         print("Error: Spreadsheet not found")
         exit(1)
-    worksheet = sh.worksheet("Sheet5")
-    worksheet.update_cell(1, 1, "Test")
+    print_info_to_sheets("Sheet5", sh, player_info)
     
 
     #match_details = get_match_details(recent_matches[0], RIOT_API_KEY)
